1912bc2b97c949e6d00fcde23743dfee9354096c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-08-10 04:13:39 +0300

    update

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i == 9 and j == 9:
+            break
+        print("{}{}, ".format(i,j), end = "")
+    if i == 9 and j == 9:
+            break
+print("{}".format(99))
