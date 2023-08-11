4d2789f6a9f992c39910df13bd697c141227e462
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-08-12 01:20:42 +0300

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # import hidden_4
-    # for i in dir("hidden_4.pyc"):
-    #     print(i)
-    print(dir("3-infinite_add.py"))
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
