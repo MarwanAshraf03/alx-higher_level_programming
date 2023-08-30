85402d7e3134009fa7cbe854415ccf3e534afa18
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-08-30 04:05:15 +0300

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Module 1."""
+
+
+class Square:
+    """Defines a Square"""
+
+    def __init__(self, size):
+        """Initializes a square with side of specific size"""
+        self.__size = size
