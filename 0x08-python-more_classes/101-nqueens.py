0f0ec9186f2894c9a4256fedccdaa4a95b161583
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-04 23:42:46 +0300

    update

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if not isinstance(sys.argv[1], int):
+    print("N must be a number")
+    exit(1)
+if sys.argv[1] < 4:
+    print("N must be at least 4")
+    exit(1)
