5bc7e189dacd725277ae748a5ce755aaa90e63b2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-04 18:34:39 +0300

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
     """
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initializing method
 
@@ -26,7 +27,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -91,4 +92,4 @@ class Rectangle:
     def __del__(self):
         """prints on deletion"""
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
