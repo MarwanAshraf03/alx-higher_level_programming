52229f64b7a751608b8100abbeb2f481333ecbe6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-06 01:33:29 +0300

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,18 +9,14 @@ def print_square(size):
     Args:
     size: the side length of square
     """
-    if type(size).__name__ in ("int"):
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be >= 0")
-        if isinstance(size, int) and size < 0:
-            raise ValueError("size must be >= 0")
-        size = int(size)
-        for i in range(size):
-            for j in range(size):
-                print("#", end="")
-            print()
-    else:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
 
 if __name__ == "__main__":
     import doctest
