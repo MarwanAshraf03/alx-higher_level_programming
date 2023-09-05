b891345cb88e3e88d45d3fc6dff7b4bf833c5822
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-05 23:05:31 +0300

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Matrix Division Module"""
+
+
 def matrix_divided(matrix, div):
     """
     Divides matrix elements with div
@@ -11,19 +13,20 @@ def matrix_divided(matrix, div):
     Return: returns new matrix consists
             of divided elements of matrix
     """
-    
+
     if type(div).__name__ not in ("int", "float"):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    s = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     if not isinstance(matrix[0], list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     if len(matrix[0]) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     len_check = all([len(i) == len(matrix[0]) for i in matrix])
     if not len_check:
         raise TypeError("Each row of the matrix must have the same size")
@@ -34,10 +37,11 @@ def matrix_divided(matrix, div):
         for counter2 in range(mat_size[1]):
             num = matrix[counter][counter2]
             if type(num).__name__ not in ("int", "float"):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(s)
             inner.append(round((matrix[counter][counter2])/div, 2))
         ret.append(inner)
     return ret
+
 
 if __name__ == "__main__":
     import doctest
