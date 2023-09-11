db9f43e78489bf18e3c007bc62747789b978d3ce
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-12 00:16:31 +0300

    update

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""My Int Module"""
+
+
 class MyInt(int):
+    """MyInt class inherits int class"""
     def __eq__(self, x):
-        return self != x
+        """overloads == to !="""
+        return super().__ne__(x)
+
     def __ne__(self, x):
-        return self == x
+        """overloads != to =="""
+        return super().__eq__(x)
