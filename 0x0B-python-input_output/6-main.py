a46aad7bc22d96159ddc84a92b992a27cbf5d02b
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-13 01:36:29 +0300

    update

--- 0x0B-python-input_output/6-main.py
+++ 0x0B-python-input_output/6-main.py
@@ -2,7 +2,7 @@
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "my_list.json"
-my_list = load_from_json_file(filename)
+my_list = list(load_from_json_file(filename))
 print(my_list)
 print(type(my_list))
 
