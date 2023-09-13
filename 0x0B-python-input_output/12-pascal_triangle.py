fd1141cd4f88f6ce96bec4cae6e30920bba3a5a2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-13 04:54:29 +0300

    update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,9 +3,8 @@ def pascal_triangle(n):
     oL = []
     if n <= 0:
         return oL
-    for i in range(1, n+1):
+    for i in range(n):
         iL = []
-        for j in range(i+1):
-            iL.append(j)
-        oL.append(iL)
+        for j in range(n):
+            iL.append(2 ** i)
     return oL
