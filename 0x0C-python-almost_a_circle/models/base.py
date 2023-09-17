1a1e4b154061d74addf3dd498e2aebb87239e18d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-17 17:58:03 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""Base Module"""
+
+
 class Base:
+    """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+        initializes Base class with id
+
+        Args:
+        id: id of the instance
+        """
         if id is not None:
             self.id = id
         else:
