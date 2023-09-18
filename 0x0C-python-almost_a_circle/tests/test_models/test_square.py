55a3ebcd9a6dca4fb38012ee6e2c5d5f07736f50
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-19 02:53:54 +0300

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -18,135 +18,126 @@ class TestSquare(unittest.TestCase):
             r = Square()
 
     def test_instantiation_with_size(self):
-        with self.assertRaises(TypeError):
-            r = Square(12)
+        r = Square(12)
+        self.assertIsInstance(r, Square)
 
     def test_instantiation_with_size_wrong(self):
         with self.assertRaises(TypeError):
-            r = Square('13', '12')
+            r = Square('13')
 
     def test_instantiation_with_size_wrong2(self):
         with self.assertRaises(ValueError):
-            r = Square(-1, '12')
+            r = Square(-1)
 
     def test_instantiation_with_size_wrong3(self):
         with self.assertRaises(TypeError):
-            r = Square(13.5, '12')
+            r = Square(13.5)
 
     def test_instantiation_with_size_wrong4(self):
         with self.assertRaises(TypeError):
-            r = Square((13.5, ), '12')
+            r = Square((13.5, ))
 
     def test_instantiation_with_size_wrong5(self):
         with self.assertRaises(TypeError):
-            r = Square([13.5], '12')
+            r = Square([13.5])
 
     def test_instantiation_with_size_wrong6(self):
         with self.assertRaises(TypeError):
-            r = Square({}, '12')
+            r = Square({})
 
     def test_instantiation_with_x_wrong(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, "2", 5)
+            r = Square(12, "2", 5)
 
     def test_instantiation_with_x_wrong2(self):
         with self.assertRaises(ValueError):
-            r = Square(12, 15, -2, 5)
+            r = Square(12, -2, 5)
 
     def test_instantiation_with_x_wrong3(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 2.5, 5)
+            r = Square(12, 2.5, 5)
 
     def test_instantiation_with_x_wrong4(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, (2, ), 5)
+            r = Square(12, (2, ), 5)
 
     def test_instantiation_with_x_wrong5(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, [2], 5)
+            r = Square(12, [2], 5)
 
     def test_instantiation_with_x_wrong6(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, {}, 5)
+            r = Square(12, {}, 5)
 
     def test_instantiation_with_y_wrong2(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 5, "2")
+            r = Square(12, 5, "2")
 
     def test_instantiation_with_y_wrong3(self):
         with self.assertRaises(ValueError):
-            r = Square(12, 15, 5, -2)
+            r = Square(12, 5, -2)
 
     def test_instantiation_with_y_wrong4(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 5, 2.5)
+            r = Square(12, 5, 2.5)
 
     def test_instantiation_with_y_wrong5(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 5, (2, ))
+            r = Square(12, 5, (2, ))
 
     def test_instantiation_with_y_wrong(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 5, [2])
+            r = Square(12, 5, [2])
 
     def test_instantiation_with_y_wrong6(self):
         with self.assertRaises(TypeError):
-            r = Square(12, 15, 5, {})
+            r = Square(12, 5, {})
 
     def test_instantiation_with_size_and_height(self):
-        r = Square(12, 13)
-        self.assertEqual(r.size, r.height - 1)
+        r = Square(12)
+        self.assertEqual(r.size, 12)
 
     def test_instantiation_full(self):
-        r = Square(12, 13, 0, 1, 14)
-        self.assertEqual(r.size, r.height - 1)
+        r = Square(12, 0, 1, 14)
+        self.assertEqual(r.size, 12)
         self.assertEqual(r.x, r.y - 1)
         self.assertEqual(r.id, 14)
 
     def test_size_getter(self):
-        r = Square(15, 16)
+        r = Square(15)
         self.assertEqual(r.size, 15)
 
     def test_size_setter(self):
-        r = Square(15, 16)
+        r = Square(15)
         r.size = 20
         self.assertEqual(r.size, 20)
 
-    def test_height_getter(self):
-        r = Square(15, 16)
-        self.assertEqual(r.height, 16)
-
-    def test_height_setter(self):
-        r = Square(15, 16)
-        r.height = 20
-        self.assertEqual(r.height, 20)
-
     def test_x_getter(self):
-        r = Square(15, 16, 3, 1)
+        r = Square(15, 3, 1)
         self.assertEqual(r.x, 3)
 
     def test_x_setter(self):
-        r = Square(15, 16, 3, 1)
+        r = Square(15, 3, 1)
         r.x = 1
         self.assertEqual(r.x, 1)
 
     def test_y_getter(self):
-        r = Square(15, 16, 0, 1)
+        r = Square(15, 0, 1)
         self.assertEqual(r.y, 1)
 
     def test_y_setter(self):
-        r = Square(15, 16, 0, 1)
+        r = Square(15, 0, 1)
         r.y = 3
         self.assertEqual(r.y, 3)
 
     def test_area_1_arg(self):
-        r = Square(10, 15)
+        r = Square(10)
         with self.assertRaises(TypeError):
             r.area(23)
 
     def test_area(self):
-        r = Square(10, 15)
-        self.assertEqual(r.area(), 150)
+        r = Square(10)
+        self.assertEqual(r.area(), 100)
 
     def test_display(self):
         pass
