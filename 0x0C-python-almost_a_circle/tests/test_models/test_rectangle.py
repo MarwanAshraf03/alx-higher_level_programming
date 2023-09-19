3a3ddb4b6e52ba3e4cc853e828fc16f7e42493a6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-19 03:06:54 +0300

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,165 +14,204 @@ class TestRectangle(unittest.TestCase):
         pass
 
     def test_instantiation_with_None(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle()
 
     def test_instantiation_with_width(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12)
 
     def test_instantiation_with_width_wrong(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle('13', '12')
 
     def test_instantiation_with_width_wrong2(self):
+        """Doc"""
         with self.assertRaises(ValueError):
             r = Rectangle(-1, '12')
 
     def test_instantiation_with_width_wrong3(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(13.5, '12')
 
     def test_instantiation_with_width_wrong4(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle((13.5, ), '12')
 
     def test_instantiation_with_width_wrong5(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle([13.5], '12')
 
     def test_instantiation_with_width_wrong6(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle({}, '12')
 
     def test_instantiation_with_height_wrong(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, '12')
 
     def test_instantiation_with_height_wrong2(self):
+        """Doc"""
         with self.assertRaises(ValueError):
             r = Rectangle(12, -1)
 
     def test_instantiation_with_height_wrong3(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 13.5)
 
     def test_instantiation_with_height_wrong4(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, (13.5,))
 
     def test_instantiation_with_height_wrong5(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, [13.5])
 
     def test_instantiation_with_height_wrong6(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, {})
 
     def test_instantiation_with_x_wrong(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, "2", 5)
 
     def test_instantiation_with_x_wrong2(self):
+        """Doc"""
         with self.assertRaises(ValueError):
             r = Rectangle(12, 15, -2, 5)
 
     def test_instantiation_with_x_wrong3(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 2.5, 5)
 
     def test_instantiation_with_x_wrong4(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, (2, ), 5)
 
     def test_instantiation_with_x_wrong5(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, [2], 5)
 
     def test_instantiation_with_x_wrong6(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, {}, 5)
 
     def test_instantiation_with_y_wrong2(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 5, "2")
 
     def test_instantiation_with_y_wrong3(self):
+        """Doc"""
         with self.assertRaises(ValueError):
             r = Rectangle(12, 15, 5, -2)
 
     def test_instantiation_with_y_wrong4(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 5, 2.5)
 
     def test_instantiation_with_y_wrong5(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 5, (2, ))
 
     def test_instantiation_with_y_wrong(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 5, [2])
 
     def test_instantiation_with_y_wrong6(self):
+        """Doc"""
         with self.assertRaises(TypeError):
             r = Rectangle(12, 15, 5, {})
 
     def test_instantiation_with_width_and_height(self):
+        """Doc"""
         r = Rectangle(12, 13)
         self.assertEqual(r.width, r.height - 1)
 
     def test_instantiation_full(self):
+        """Doc"""
         r = Rectangle(12, 13, 0, 1, 14)
         self.assertEqual(r.width, r.height - 1)
         self.assertEqual(r.x, r.y - 1)
         self.assertEqual(r.id, 14)
 
     def test_width_getter(self):
+        """Doc"""
         r = Rectangle(15, 16)
         self.assertEqual(r.width, 15)
 
     def test_width_setter(self):
+        """Doc"""
         r = Rectangle(15, 16)
         r.width = 20
         self.assertEqual(r.width, 20)
 
     def test_height_getter(self):
+        """Doc"""
         r = Rectangle(15, 16)
         self.assertEqual(r.height, 16)
 
     def test_height_setter(self):
+        """Doc"""
         r = Rectangle(15, 16)
         r.height = 20
         self.assertEqual(r.height, 20)
 
     def test_x_getter(self):
+        """Doc"""
         r = Rectangle(15, 16, 3, 1)
         self.assertEqual(r.x, 3)
 
     def test_x_setter(self):
+        """Doc"""
         r = Rectangle(15, 16, 3, 1)
         r.x = 1
         self.assertEqual(r.x, 1)
 
     def test_y_getter(self):
+        """Doc"""
         r = Rectangle(15, 16, 0, 1)
         self.assertEqual(r.y, 1)
 
     def test_y_setter(self):
+        """Doc"""
         r = Rectangle(15, 16, 0, 1)
         r.y = 3
         self.assertEqual(r.y, 3)
 
     def test_area_1_arg(self):
+        """Doc"""
         r = Rectangle(10, 15)
         with self.assertRaises(TypeError):
             r.area(23)
 
     def test_area(self):
+        """Doc"""
         r = Rectangle(10, 15)
         self.assertEqual(r.area(), 150)
 
     def test_display(self):
+        """Doc"""
         pass
 
 
