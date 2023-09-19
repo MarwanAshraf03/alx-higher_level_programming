1820dc047ba3e98b9c35e59fdf85e28d71843257
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-19 04:54:22 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns JSON encoding of list_dictionaries"""
-        if list_dictionaries is None or not list_dictionaries:
+        if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
