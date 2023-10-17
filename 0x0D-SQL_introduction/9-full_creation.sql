ddd8dc23fe3392149f2190e1a8b347057b853ead
root <marawanashraf556.study@gmail.com>
2023-10-17 06:50:31 -0700

    update

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- a script that creates a table second_table in the database
+CREATE TABLE IF NOT EXISTS second_table(
+	INT,
+	VARCHAR(256),
+	INT);
+INSERT INTO TABLE second_table VALUES(1, 'john', 10), (), (), ();
+1, "John", score = 10
+id = 2, name = “Alex”, score = 3
+id = 3, name = “Bob”, score = 14
+id = 4, name = “George”, score = 8
