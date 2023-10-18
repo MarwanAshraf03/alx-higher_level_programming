4271616d1a815c3859778aaee95cfc250b4b1d66
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-10-18 23:22:34 +0300

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 --
-select tv_shows.title, tv_show_genres.genre_id from tv_shows join tv_show_genres on tv_shows.id = tv_show_genres.show_id order by tv_shows.title,tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title,tv_show_genres.genre_id;
