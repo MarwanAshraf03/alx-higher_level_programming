8d912185f5c41cbcb69fc97c540bb006294c9d12
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-13 11:23:16 +0200

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (!process.argv[2] && !process.argv[3]) {
-  console.log('undefined is undefined');
-} else if (!process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + 'undefined');
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
