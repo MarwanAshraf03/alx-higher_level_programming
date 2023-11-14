b144f00aed1d939968915d4a9b6a2363fcb43f8e
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-14 20:12:35 +0200

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Square2 = require('./5-square');
+class Square extends Square2 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      let str1 = '';
+      for (let i = 0; i < this.height; i++) {
+        let str2 = '';
+        for (let j = 0; j < this.width; j++) {
+          str2 += c;
+        }
+        if (i === this.height - 1) {
+          str1 += str2;
+        } else {
+          str1 += str2 + '\n';
+        }
+      }
+      console.log(str1);
+    }
+  }
+}
+module.exports = Square;
