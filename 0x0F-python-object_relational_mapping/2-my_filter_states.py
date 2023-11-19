a44d7e15c117b60943e4fc794ea6a615cc55dd89
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 16:10:35 +0200

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ def main():
         port=3306,
         user=argss[0], passwd=argss[1], db=argss[2])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE\
-    name LIKE '{}' ORDER BY states.id ASC".format(argss[3])
+    query = """SELECT * FROM states WHERE
+    name LIKE '{}' ORDER BY states.id ASC""".format(argss[3])
     cur.execute(query)
     for row in cur.fetchall():
         print(row)
