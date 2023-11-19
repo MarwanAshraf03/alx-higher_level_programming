75026fb2437778546cbf76a0872136908b978d6c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 21:44:16 +0200

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,7 +8,10 @@ Base = declarative_base()
 class State(Base):
     """Creates table states in hbtn_0e_6_usa"""
     __tablename__ = "states"
-    id = Column("id", Integer, autoincrement=True, primary_key=True,
+    id = Column("id", Integer,
+                primary_key=True,
+                unique=True,
+                autoincrement=True,
                 nullable=False)
     name = Column("name", String(128), nullable=False)
 
