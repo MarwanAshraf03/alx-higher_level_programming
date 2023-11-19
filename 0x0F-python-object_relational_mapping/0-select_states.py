76becd8d933fc4b1b720b90a21954fffa8c7b5c5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 14:43:35 +0200

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-import MySQLdb
-import sqlalchemy
-print(MySQLdb.version_info)
-print(sqlalchemy.__version__)
+"""A python module to print states from a database"""
+
+def main():
+    import MySQLdb
+    import sys
+    argss = sys.argv[1:]
+
+    db = MySQLdb.Connect(host="localhost", user=argss[0], passwd=argss[1], db=argss[2])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
+if __name__ == "__main__":
+    main()
