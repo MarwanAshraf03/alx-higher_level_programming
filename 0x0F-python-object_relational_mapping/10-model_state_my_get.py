3b6bd1d8b9e009f8c736c2362393e2e62b08c92f
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 22:43:54 +0200

    update

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
                            .format(argv[1],
                                    argv[2],
                                    argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
     session = Session()
     row = session.query(State.id).filter(State.name == argv[4])\
