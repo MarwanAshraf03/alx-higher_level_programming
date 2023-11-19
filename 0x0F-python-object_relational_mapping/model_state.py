d918ee40b96684a02e3ff8b8e1c7015818d7ba8d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 21:40:39 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module that creates table states in hbtn_0e_6_usa"""
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """Creates table states in hbtn_0e_6_usa"""
+    __tablename__ = "states"
+    id = Column("id", Integer, autoincrement=True, primary_key=True,
+                nullable=False)
+    name = Column("name", String(128), nullable=False)
+
+
+engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa',
+                       pool_pre_ping=True)
+Base.metadata.create_all(bind=engine)
