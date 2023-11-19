7d1a37158e3f87e79a0dd572156aa8cb4f10b4e5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 15:53:36 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A python module to print
+states which has N as a first letter in
+their name from a database"""
+
+
+def main():
+    import MySQLdb
+    import sys
+    argss = sys.argv[1:]
+
+    db = MySQLdb.Connect(
+        host="localhost", user=argss[0], passwd=argss[1], db=argss[2])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
