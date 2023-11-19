b613546f9bdfc8c5210e1bac6f20fc3449cbf2ae
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-19 22:02:29 +0200

    update

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    argv[1],
+    argv[2],
+    argv[3]),
+    pool_pre_ping=True)
+
+Session = sessionmaker(engine)
+session = Session()
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A module that creates table states in hbtn_0e_6_usa"""
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
@@ -14,8 +14,3 @@ class State(Base):
                 autoincrement=True,
                 nullable=False)
     name = Column("name", String(128), nullable=False)
-
-
-# engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa',
-#                        pool_pre_ping=True)
-# Base.metadata.create_all(bind=engine)
