7b62febcf60478d6d35d8249b0a92c35ceb6489c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-02 17:05:56 +0200

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A module to find pead of list"""
+
+
 def find_peak(list_of_integers):
     """the method to find the peak of the list"""
     if (len(list_of_integers)) == 0:
