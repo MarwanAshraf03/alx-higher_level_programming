ff53ba2a2ffb443cfb92b97405cf5cc9c0353d94
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-05 17:40:53 +0200

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,10 @@ def find_peak(list_of_integers):
     """the method to find the peak of the list"""
     if (len(list_of_integers)) == 0:
         return None
-    return max(list_of_integers)
+    if len(list_of_integers) > 1:
+        return max(list_of_integers[0], find_peak(list_of_integers[1:]))
+    else:
+        return list_of_integers[0]
+
+def max(x, y):
+    return x if x > y else y
