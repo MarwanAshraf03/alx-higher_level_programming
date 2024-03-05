8e7a523b4746dd7c6704323db2a2ee50d3ecabe2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:35:55 +0200

    update

--- 0x11-python-network_1/try.py
+++ 0x11-python-network_1/try.py
@@ -1,8 +1,11 @@
-# """To fetch https://alx-intranet.hbtn.io/status"""
-# from urllib import request
-# import sys
-
-# try:
-#     with request.urlopen("http://0.0.0.0:5000/status_401") as response:
-#         print(dir(request))
-# except urllib.error
+"""Find X-Request-Id value"""
+if __name__ == "__main__":
+    from urllib import request
+    from urllib import error
+    import sys
+    try:
+        with request.urlopen("https://www.google.com/404") as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(dir(e))
+        print(e.code)
