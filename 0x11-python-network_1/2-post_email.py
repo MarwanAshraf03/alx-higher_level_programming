a76828c37e2acebcf04edcf23257a4c48c29edbf
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:13:23 +0200

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,5 +4,10 @@ if __name__ == "__main__":
     from urllib import request
     from urllib import parse
     import sys
-    with request.urlopen(request.Request(sys.argv[1], parse.urlencode(sys.argv[2].encode('ascii')))) as response:
-        print(response.read().decode('utf-8'))
+    data = parse.urlencode(sys.argv[2])
+    data = data.encode('ascii') # data should be bytes
+    req = request.Request(sys.argv[0], data)
+    with request.urlopen(req) as response:
+        the_page = response.read()
+    # with request.urlopen(request.Request(sys.argv[1], parse.urlencode(sys.argv[2].encode('ascii')))) as response:
+    #     print(response.read().decode('utf-8'))
