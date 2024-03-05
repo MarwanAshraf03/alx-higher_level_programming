b8009e171c2e1ab8b69ee3f5ed41da3e0f4e4d7f
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:17:41 +0200

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
     import sys
     values = {'email': sys.argv[2]}
     data = parse.urlencode(values)
-    data = data.encode('ascii') # data should be bytes
+    data = data.encode('ascii')
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
-    # with request.urlopen(request.Request(sys.argv[1], parse.urlencode(sys.argv[2].encode('ascii')))) as response:
-    #     print(response.read().decode('utf-8'))
