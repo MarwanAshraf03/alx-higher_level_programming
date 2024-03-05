3bf8a9d433a893b71d8a06381bdc259208b774f9
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:55:15 +0200

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import requests
     import sys
     resp = requests.get(sys.argv[1])
-    if resp.status_code > 400:
+    if resp.status_code >= 400:
         print(f"Error code: {resp.status_code}")
     else:
         print(resp.content.decode('utf-8'))
