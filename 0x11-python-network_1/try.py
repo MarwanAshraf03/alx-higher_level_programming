84376beedd0b6db2391b852cf87a9ee03573d472
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:32:28 +0200

    update

--- 0x11-python-network_1/try.py
+++ 0x11-python-network_1/try.py
@@ -1,12 +1,8 @@
-"""To fetch https://alx-intranet.hbtn.io/status"""
-from urllib import request
-import sys
+# """To fetch https://alx-intranet.hbtn.io/status"""
+# from urllib import request
+# import sys
 
-with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    # for i in response.getheaders():
-    #     if i[0] == 'X-Request-Id':
-    #         print(i[1])
-    # resp = response.read()
-    print(dir(request))
-    # print(dir(response))
-# print(response.getheaders())
+# try:
+#     with request.urlopen("http://0.0.0.0:5000/status_401") as response:
+#         print(dir(request))
+# except urllib.error
