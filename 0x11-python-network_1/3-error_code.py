3dd736bc947dc771838e656dceb84fc4eebe86b2
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:37:06 +0200

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(e)
+        print(f"Error code: {e.code}")
