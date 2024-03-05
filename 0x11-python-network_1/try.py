ef77d4eb7303fc1bdef2c8659f39676058d97b49
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-05 23:58:58 +0200

    1-hbtn_header.py

--- 0x11-python-network_1/try.py
+++ 0x11-python-network_1/try.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
 """To fetch https://alx-intranet.hbtn.io/status"""
 from urllib import request
 import sys
-with request.urlopen(sys.argv[1]) as response:
+with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     for i in response.getheaders():
         if i[0] == 'X-Request-Id':
             print(i[1])
+    # resp = response.read()
+# print(dir(response))
+# print(response.getheaders())
