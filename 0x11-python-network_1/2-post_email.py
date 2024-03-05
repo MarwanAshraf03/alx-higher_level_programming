e7df774761bede7520068978417fd83bf5ed05a5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:11:32 +0200

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,6 +2,7 @@
 """Find X-Request-Id value"""
 if __name__ == "__main__":
     from urllib import request
+    from urllib import parse
     import sys
-    with request.urlopen(request.Request(sys.argv[1], sys.argv[2].encode('ascii'))) as response:
+    with request.urlopen(request.Request(sys.argv[1], parse.urlencode(sys.argv[2].encode('ascii')))) as response:
         print(response.read().decode('utf-8'))
