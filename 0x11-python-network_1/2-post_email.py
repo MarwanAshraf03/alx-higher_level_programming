8838894e83151e9bc98ddd53f00dadb7c0f601f5
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:17:05 +0200

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     data = data.encode('ascii') # data should be bytes
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
-        the_page = response.read()
+        print(response.read().decode('utf-8'))
     # with request.urlopen(request.Request(sys.argv[1], parse.urlencode(sys.argv[2].encode('ascii')))) as response:
     #     print(response.read().decode('utf-8'))
