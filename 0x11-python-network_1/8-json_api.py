3bf4436c804821a82ee633a4bd3d4b772c85e99d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 00:59:25 +0200

    update

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Find X-Request-Id value"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    if len(sys.argv) > 1:
+        values = {'q': sys.argv[1]}
+    else:
+        values = {'q': ""}
+    resp = requests.post("http://0.0.0.0:5000/search_user", values)
+    print(resp.content)
+    print(resp.content.decode('utf-8'))
