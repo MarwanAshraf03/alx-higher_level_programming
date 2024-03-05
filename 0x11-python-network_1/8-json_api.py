60c61407fec2a2710197fd93b8b48e29e3e0d02a
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-06 01:03:29 +0200

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         values = {'q': ""}
     resp = requests.post("http://0.0.0.0:5000/search_user", values)
     print(dir(resp.json))
-    print(dir(resp.json))
+    print(resp.json)
     # print(resp.content)
     # print(type(resp.content.decode('utf-8')))
     # print(resp.content.decode('utf-8'))
