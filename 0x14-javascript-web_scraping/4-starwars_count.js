2530c7f17b05ba1eee00de693cedabd064fb4042
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-27 03:37:58 +0200

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,12 @@ request(process.argv[2], function (error, response, body) {
   if (error) throw error;
   const b = JSON.parse(body);
   let number = 0;
-  console.log(b.results[0].characters.length)
   for (let i = 0; i < b.count; i++) {
     for (let j = 0; j < b.results[i].characters.length; j++) {
       if (b.results[i].characters[j].includes('/18/')) {
         number++;
       }
     }
-    // if (b.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-    //   number++;
   }
-  // }
   console.log(number);
 });
