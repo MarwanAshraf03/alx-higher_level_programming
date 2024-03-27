d0bafee7a2b0df9bc6c3985bfb165833d9cd2209
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-27 14:04:20 +0200

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
-const filter = 18;
-request(process.argv[2], function (error, response, body) {
-  if (error) throw error;
-  const b = JSON.parse(body);
-  let number = 0;
-  for (let i = 0; i < b.count; i++) {
-    for (let j = 0; j < b.results[i].characters.length; j++) {
-      if (b.results[i].characters[j].includes(filter)) {
-        number++;
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (res.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+async function main () {
+  request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], async function (error, response, body) {
+    if (error) throw error;
+    const b = JSON.parse(body);
+    for (let i = 0; i < b.characters.length; i++) {
+      try {
+        const response = await doRequest(b.characters[i]);
+        console.log(JSON.parse(response).name);
+      } catch (error) {
+        console.error(error);
       }
     }
-  }
-  console.log(number);
-});
+  });
+}
+main();
