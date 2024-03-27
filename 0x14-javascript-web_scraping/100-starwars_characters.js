e1b1a59c1b72a3563870d98dab979a0548ce18c4
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-03-27 04:14:23 +0200

    100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const filter = 18;
-request("https://swapi-api.alx-tools.com/api/films/"+process.argv[2], function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   if (error) throw error;
   const b = JSON.parse(body);
-  console.log(b)
-    // for (let j = 0; j < b.results[i].characters.length; j++) {
-    //   if (b.results[i].characters[j].includes(filter)) {
-    //   }
-    // }
+  for (let j = 0; j < b.characters.length; j++) {
+    request(b.characters[j], function (error, response, bb) {
+      if (error) throw error;
+      console.log(JSON.parse(bb).name);
+    });
+  }
 });
